d2ff7add7d2b7368d4730c2435e81bc226abac4d
gautamSH20 <gs841400@gmail.com>
2025-03-18 21:14:42 +0530

    database added

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,15 +1,23 @@
 import { NextResponse, NextRequest } from "next/server";
+import client from "@/app/lib/db";
 
-export function GET() {
+export async function GET() {
+  const data = await client.user.findFirst({});
   return NextResponse.json({
-    name: "gautam",
-    email: "gs8515000@gmail.com",
+    name: data?.username,
+    password: data?.password,
   });
 }
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
   console.log(data);
+  await client.user.create({
+    data: {
+      username: data.username,
+      password: data.password,
+    },
+  });
   return NextResponse.json({
     message: "you are there",
   });
